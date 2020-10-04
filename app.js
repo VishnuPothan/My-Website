f36744a8ef7f43a08b468ad74450b962d9313968
Vishnu <vishnupothan2@gmail.com>
2020-10-04 21:51:53 +0530

    String interpolation opporator added.

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 require('dotenv').config();
 
+//mongodb connect
 const mongoDBUserName = process.env.MONGODB_USERNAME;
 const mongoDBPassword = process.env.MONGODB_PASSWORD;
 const mongoDBUrl =  `mongodb+srv://${mongoDBUserName}:${mongoDBPassword}@cluster0.cxmpb.gcp.mongodb.net/RepositoryDetails?retryWrites=true&w=majority`;
-
 mongoose.connect(mongoDBUrl ,{ useUnifiedTopology: true , useNewUrlParser: true});
 
 //schema setup
@@ -16,18 +16,6 @@ let repoSchema = new mongoose.Schema({
 });
 
 let  RepoDetails = mongoose.model("RepoDetails" , repoSchema);
-RepoDetails.create(
-    {
-        name: "Services",
-        description: "Som  e random project" 
-    },(err,RepoDetails)=>{
-        if(err){
-            console.log(err);
-        } else{
-            console.log("Created new DB");
-            console.log(RepoDetails);
-        }
-    });
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
