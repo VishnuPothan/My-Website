06300c8a8f05f0642296ddc58a96a0d4f2ac87c2
Vishnu <vishnupothan2@gmail.com>
2020-10-04 20:05:28 +0530

    mongoose added

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://vishnu:iT7dYh2bmz7AHVj7@cluster0.cxmpb.gcp.mongodb.net/RepositoryDetails?retryWrites=true&w=majority",{ useUnifiedTopology: true , useNewUrlParser: true});
+
+//schema setup
+let repoSchema = new mongoose.Schema({
+    name: String,
+    description: String
+});
+
+let  RepoDetails = mongoose.model("RepoDetails" , repoSchema);
+RepoDetails.create(
+    {
+        name: "Quizzer",
+        description: "Som  e random project" 
+    },(err,RepoDetails)=>{
+        if(err){
+            console.log(err);
+        } else{
+            console.log("Created new DB");
+            console.log(RepoDetails);
+        }
+    });
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
